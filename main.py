3962b3c54b12de44a819ab034ff6d65d25bc9d4a
Sanya <160408798+LovkiiSanya@users.noreply.github.com>
2024-04-24 18:47:41 +0200

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Message import * 
+from Message import Message,message_dict
 from AudioMessage import *
 from VideoMessage import *
 from support import *
@@ -14,21 +14,4 @@ while True:
         main()  
     CRUDoperations()
     
-    try:
-        connection = psycopg2.connect(
-        host="127.0.0.1", user="admin", password="root", dbname="postgres"
-    )
-        connection.autocommit = True
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT version();")
-            print(cursor.fetchone())
 
-        with connection.cursor() as cursor:
-            cursor.execute("""INSERT INTO telega (message_type) VALUES ('message') """)
-
-    except Exception as _ex:
-        print("[INFO] Ошибка при работе с Базой данных", _ex)
-    finally:
-        if connection:
-            connection.close()
-            print("[INFO] Соединение с Базой данных закрыто.")
