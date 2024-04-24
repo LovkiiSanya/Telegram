2f122044a30fe8dbd02126f829026fbda1fc465a
Sanya <160408798+LovkiiSanya@users.noreply.github.com>
2024-04-24 18:47:07 +0200

    create_db.py

--- create_db.py
+++ create_db.py
@@ -21,12 +21,47 @@ class BaseModel(Model):
         database = connection
 
 class TelegaTable(BaseModel):
-    #id_message = IntegerField(column_name="id") 
     message_table_type = TextField(column_name="message_type") #формат сообщения
     message_table_content = TextField(column_name="message_content") #содержимое сообщения
     message_table_recipient = TextField(column_name="message_recipient") #имя получателя
-    message_table_date = TimeField(column_name="message_date") #дата отправки
+    message_table_date = DateTimeField(column_name="message_date") #дата отправки
     message_table_duration = IntegerField(column_name="message_duration") #длительность сообщения (аудио или видео)
     message_table_quality = TextField(column_name="message_quality") #качество сообщения (видео)
     class Meta:
         table_name = "telega"
+
+class BasicTextMessage(TelegaTable):
+    def __init__(self, content, recipient, time, message_type='text') -> None:
+        super().__init__(
+            message_table_type=message_type,
+            message_table_content=content,
+            message_table_recipient=recipient,
+            message_table_date=time,
+        )
+        self.save()  # Сохранение записи в базе данных
+
+
+
+class BasicAudioMessage(TelegaTable):
+    def __init__(self, content, recipient, time,duration, message_type) -> None:
+        super().__init__(
+            message_table_type=message_type,
+            message_table_content=content,
+            message_table_recipient=recipient,
+            message_table_date=time,
+            message_table_duration = duration
+        )
+        self.save()  # Сохранение записи в базе данных
+
+class BasicVideoMessage(TelegaTable):
+    def __init__(self, content, recipient, time,duration, message_type,quality) -> None:
+        super().__init__(
+            message_table_type=message_type,
+            message_table_content=content,
+            message_table_recipient=recipient,
+            message_table_date=time,
+            message_table_duration = duration,
+            message_table_quality = quality
+
+        )
+        self.save()  # Сохранение записи в базе данных
